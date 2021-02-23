dbcde16dd9d016c3f7bb13e090dac19425ee09f6
Eric Leuty <16eal@caslab.queensu.ca>
2021-02-22 19:21:22 -0500

    I added multiple html pages

--- main.js
+++ main.js
@@ -4,7 +4,15 @@ const fs = require('fs');
 const app = express();
 
 app.get('/', (req, res) => {
-   res.sendFile('/Users/ericleuty/WebstormProjects/personal_website/demofile.html')
+   res.sendFile('/Users/ericleuty/WebstormProjects/personal_website/home.html')
+});
+
+app.get('/skills', (req, res) => {
+   res.sendFile('/Users/ericleuty/WebstormProjects/personal_website/skills.html')
+});
+
+app.get('/work', (req, res) => {
+   res.sendFile('/Users/ericleuty/WebstormProjects/personal_website/work.html')
 });
 
 const server = app.listen(8080, () => {
