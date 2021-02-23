4ec2c82f0f088c67b6cbfdcebdc5de4f75b9b80a
Eric Leuty <16eal@caslab.queensu.ca>
2021-02-22 20:03:09 -0500

    fixed addresses

--- main.js
+++ main.js
@@ -4,15 +4,15 @@ const fs = require('fs');
 const app = express();
 
 app.get('/', (req, res) => {
-   res.sendFile('/Users/ericleuty/WebstormProjects/personal_website/home.html')
+   res.sendFile('/home/eric/personal_website/home.html')
 });
 
 app.get('/skills', (req, res) => {
-   res.sendFile('/Users/ericleuty/WebstormProjects/personal_website/skills.html')
+   res.sendFile('/home/eric/personal_website/skills.html')
 });
 
 app.get('/work', (req, res) => {
-   res.sendFile('/Users/ericleuty/WebstormProjects/personal_website/work.html')
+   res.sendFile('/home/eric/personal_website/work.html')
 });
 
 const server = app.listen(8080, () => {
