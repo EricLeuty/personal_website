414c7028ee6338f560b3cace770c57a3cd53b950
Eric Leuty <16eal@caslab.queensu.ca>
2021-02-22 21:18:32 -0500

    I did some funny stuff

--- main.js
+++ main.js
@@ -22,6 +22,10 @@ app.get('/style.css', (req, res) => {
 app.get('/stella.png', (req, res) => {
     res.sendFile('/home/eric/personal_website/stella.png')
 });
+
+app.get('/flowers.jpeg', (req, res) => {
+    res.sendFile('/home/eric/personal_website/flowers.jpeg')
+});
  
 const server = app.listen(8080, () => {
    console.log(`Express running -> PORT ${server.address().port}`);
